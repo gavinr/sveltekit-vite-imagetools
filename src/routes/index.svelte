c028e24cece9e44cf7186db027b757d451abeb17
Gavin Rehkemper <gavin@gavinr.com>
2021-08-08 23:14:22 -0500

    basic - working as expected

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,8 @@
+<script>
+	import Image from '../../static/1.jpg?w=400&h=300&webp';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<img src={Image} alt="test1" />
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+import { imagetools } from 'vite-imagetools';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			plugins: [imagetools()]
+		}
 	}
 };
 
